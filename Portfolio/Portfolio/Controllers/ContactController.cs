c98d9364f14cbbc3517717d50d6532e85c02e168
SeedPaths-User <lessdismissive@email.com>
2014-09-29 12:48:22 -0600

    Portfolio'd

---
+++ Portfolio/Portfolio/Controllers/ContactController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Portfolio.Controllers
+{
+    public class ContactController : Controller
+    {
+        //
+        // GET: /Contact/
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View(new Models.ContactMeForm());
+        }
+
+        [HttpPost]
+        public ActionResult Index(Models.ContactMeForm contact)
+        {
+            Models.ContactMeEntities db = new Models.ContactMeEntities();
+            db.ContactMeForms.Add(contact);
+            //save changes
+            db.SaveChanges();
+            //kick user back to list
+            return RedirectToAction("ThankYou", "Contact");
+        }
+
+        public ActionResult ThankYou()
+        {
+            return View();
+        }
+    }
+}
---
+++ Portfolio/Portfolio/Models/ContactMeFormValidation.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Portfolio.Models
+{
+    //Step 2: Make a partial class for class we're trying to validate
+    //Step 3: Add data annotation for the metadataType to the validation class
+    [MetadataType(typeof(ContactMeFormValidation))]
+    public partial class ContactMeForm
+    {
+    }
+    public class ContactMeFormValidation
+    {
+        //Step 4: Declare the properties of the class you want to validate, set data annotations
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required, EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        public string ContactNumber { get; set; }
+        [Required]
+        public string Comment { get; set; }
+    }
+}
