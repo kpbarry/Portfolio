3f74bae7efa706f18f06c6c60c08906bdb478c44
SeedPaths-User <lessdismissive@email.com>
2014-10-06 16:56:50 -0600

    Fixing work page

--- Portfolio/Portfolio/Scripts/main.js
+++ Portfolio/Portfolio/Scripts/main.js
@@ -14,6 +14,21 @@
         //Remove the hide class, add active class to next slide
         nextSlide.removeClass('hide').addClass('active');
     });
+
+    $('.carousel-prev').on('click', function () {
+        //Get the active slide
+        var activeSlide = $(this).parent().find('.carousel.active');
+        //Get the next slide
+        var nextSlide = activeSlide.prev();
+        //Make sure it's a carousel slide
+        if (!nextSlide.hasClass('carousel')) {
+            nextSlide = $(this).parent().find('.carousel').last();
+        }
+        //Remove active class, add hide class to active slide
+        activeSlide.removeClass('active').addClass('hide');
+        //Remove the hide class, add active class to next slide
+        nextSlide.removeClass('hide').addClass('active');
+    });
     //Carousel Functions END
 
     //SUPERIOR AJAX GET 
